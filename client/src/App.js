237c772d8e10e35be61aba41d3935739dda1990a
shyrapson <meetshyrapson@gmail.com>
2022-09-21 22:19:20 +0100

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Pay from "./pages/Pay";
 import Success from "./pages/Success";
-import './App.css'
+import "./App.css";
+import { Home, Product, ProductList, Login, Register } from "./pages";
 
 const App = () => {
   return (
     <div>
       <Routes>
+        <Route path="/product" element={<Product />} />
+        <Route path="/" element={<Home />} />
         <Route path="/pay" element={<Pay />} />
         <Route path="/success" element={<Success />} />
       </Routes>
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home";
+export { default as Product } from "./Product";
+export { default as ProductList } from "./ProductList";
+export { default as Register } from "./Register";
+export { default as Login } from "./Login";
+export { default as Cart } from "./Cart";
